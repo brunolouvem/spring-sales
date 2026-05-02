f75ea6c31bf09a11ae2c7ee02fb9bce7322b037d
Bruno Louvem <btlouvem@gmail.com>
2026-05-02 19:34:18 +0300

    Initial commit: Spring Sale website

---
+++ catalog.js
@@ -0,0 +1,67 @@
+const items = [
+  {
+    name: "Gimbal DJI RS 3 Mini + Smallrig adapter",
+    description: "Gimbal DJI RS 3 Mini + Smallrig adapter. Original price: €225,00",
+    price: 190,
+    currency: "€",
+    images: [
+      "assets/images/gimbal-dji-rs3-mini-1.jpeg",
+      "assets/images/gimbal-dji-rs3-mini-2.jpeg",
+      "assets/images/gimbal-dji-rs3-mini-3.jpeg",
+      "assets/images/gimbal-dji-rs3-mini-4.jpeg"
+    ],
+    storeUrl: "https://www.amazon.de/-/en/dp/B0BLD1KBHQ?ref=ppx_yo2ov_dt_b_fed_asin_title&th=1",
+    storeName: "amazon.de",
+  },
+  {
+    name: "PGYTECH MANTISPOD PRO Mini",
+    description: "PGYTECH MANTISPOD PRO Mini. Original price: €70,00. Note: This <a href='https://www.amazon.de/-/en/PGYTECH-MANTISPOD-2-0-Classic-Black/dp/B0BN78RKRN/ref=sr_1_21_opt_CAMERA_TRIPOD?crid=I8YCZ9Y0ZZ1Q&dib=eyJ2IjoiMSJ9.lCYUQbXWzzCXQXLmyWKbNJguA9hnCcKW6cnm0lWZ5D5uFL2eSjkRdoWK0bqE5zN0BlifgWTcioLqyG_I9MN8BO5Y_b1rjvLHYGT_2-5jHCPQWk9FwtfjLft4GYMVsw5ZKQ8HitcWoqDelVij2wF2BsKwJrq-3WBWyq8fran0LUpVV9xCG2W7ltR0z5nbXeWoM17vBsRhsogcFDioiq5EOF9L6iibpSka3hqFCTkcpb0.XA_HpLJv7nhgKpuMZ8AzZAQq1Ka3hu2LL4qcgmbOxB0&dib_tag=se&keywords=PGYTECH+MANTISPOD+PRO+Mini&qid=1777738458&s=ce-de&sprefix=pgytech+mantispod+pro+mini%2Celectronics%2C148&sr=1-21' target='_blank'>model</a> was discontinued in 2025, so we are using the new version, the MANTISPOD 2",
+    price: 50,
+    currency: "€",
+    images: [
+      "assets/images/pgytech-support-1.jpeg"
+    ],
+    storeUrl: "https://www.amazon.de/-/en/PGYTECH-MANTISPOD-Vlogging-Flexible-Shooting/dp/B0BKFLH1H8?ref_=ast_sto_dp&th=1",
+    storeName: "amazon.de",
+  },
+  {
+    name: "Canon EOS250D",
+    description: "Canon EOS250D. Original price: €728,00",
+    price: 600,
+    currency: "€",
+    images: [
+      "assets/images/camera-canon-eos-250d-1.jpeg",
+      "assets/images/camera-canon-eos-250d-2.jpeg",
+      "assets/images/camera-canon-eos-250d-3.jpeg",
+      "assets/images/camera-canon-eos-250d-4.jpeg",
+      "assets/images/camera-canon-eos-250d-5.jpeg"
+    ],
+    storeUrl: "https://www.amazon.de/-/en/Canon-EOS-250D-Case-f4-5-6/dp/B07QS1PYSB/ref=sr_1_2?crid=1OLH0X5I8VTQP&dib=eyJ2IjoiMSJ9.pf0c1LhIfDEKRBmi2cFCXnVK7V395w3JbHlrwG2-r2_1I7684fRN0KqfROS4KaI5VCr8kQ3gKpC9h7DFTkLo1wpI0fzdKB8PkZcjfWh6QzE430Gtkv4q_U44oscuDIxhjbMv5BJ8KyhURtVzqZbbbWje0mCmS8SiRYW5V-hNX_KEkfBDr6VmU_BZiy9jf0QntmdpKvGoyVZduWQrl5F4VeuY0AujlY6aJlsim1DzqWE.hb2UjGTivU2Ep5DSr1motPhThTaAPcps3H3mq7qAN18&dib_tag=se&keywords=Canon%2BEOS250D&qid=1777311836&sprefix=canon%2Beos250d%2Caps%2C149&sr=8-2&th=1",
+    storeName: "amazon.de",
+  },
+  {
+    name: "Rode VideoMic ProPlus + deadcat",
+    description: "Rode VideoMic ProPlus + deadcat. Original price: €260,00 (Mic 213,00 + deadcat 47,00). Note: The deadcat is original Rode and the buy link could be reached <a href='https://www.amazon.de/-/en/R%C3%98DE-DEADCATVMP-DeadCat-VMP/dp/B07D14L3LH/ref=sr_1_7?dib=eyJ2IjoiMSJ9.sEbfz0VLUBBOmONACoIjqI0I-qT3Yhx0FY2lXWD-4LPHQFTaZ4ESdjw4Zog-s-zIq6q-hPubMcM5tUY7fzQgxl5xzLZs4MBM7LccXkerUR2bHKQM-m2rqxdWlz612KMnyElEq82kdZ7am7nEBTwXNM6JP3EVswEuzfYDgj2a2pkjxU4mjgcHblbZMWdRCEu0DbX-0hpVvMnsOJv7U2yoFBOWuGMQQG9ST6k8XPu0QpWalKq734pLsey9M8Ga1qot2CD6RbZCiywWTf0uFTEGM1RlYGKTzqjYW2tLoduo4HU.5IbToeeF_Vu7lJ3Du897CSqvpoaNhHwIyROP4eicLI4&dib_tag=se&keywords=Rode%2Bmicrophone%2BVideoMic%2BPro%2B%2Bdeadcat&qid=1777739192&sr=8-7&th=1' target='_blank'>here</a>.",
+    price: 210,
+    currency: "€",
+    images: [
+      "assets/images/mic-rode-with-deadcat-1.jpeg",
+      "assets/images/mic-rode-with-deadcat-2.jpeg"
+    ],
+    storeUrl: "https://www.amazon.de/-/en/R%C3%98DE-Microphone-High-Pass-High-Frequency-Filmmaking/dp/B0749GV5L3/ref=sr_1_2?crid=CLHFBK0EIHGY&dib=eyJ2IjoiMSJ9.m6BvkygnckaDK_QfoJniW3zO6Sh2HY5c0257cpYYym8T48UTDuIzznPd94em603jL87wLjOj2MAGJSUJCDrHMONZABWyLCFNxDYd0yhytmLrs8ZxP9zAMzrPip-RX4HoXe45fIFGo7s4mXbFITuOdyUlDTc68X48QtFEBItH3izfoCwCmAW4oR4jLoe_SPghJb7YR4Ij7iJrDo3Kau2J_KPoOY90vGqiiJT3gOLXBnWmT4h3Sa6F-n7FDtkdZ7Qul2SNY2jj97XFAZOphQsUFqvlUFaw7xgABvRz3N4iki8.Vq5LkzrCj7PAoKPFdUU4upH2Jr99dGED7JtLgKguP3M&dib_tag=se&keywords=Rode%2Bmicrophone%2BVideoMic%2BPro%2B&qid=1777312538&s=ce-de&sprefix=rode%2Bmicrophone%2Bvideomic%2Bpro%2B%2Celectronics%2C216&sr=1-2&th=1",
+    storeName: "amazon.de",
+  },
+  {
+    name: "Nacon daija arcade stick ps5",
+    description: "Nacon daija arcade stick ps5. Original price: €225,00",
+    price: 180,
+    currency: "€",
+    images: [
+      "assets/images/arcade-stick-nacon-daija-1.jpeg",
+      "assets/images/arcade-stick-nacon-daija-2.jpeg",
+      "assets/images/arcade-stick-nacon-daija-3.jpeg"
+    ],
+    storeUrl: "https://gameroom.ee/et/playstation-puldid/products/daija-arcade-fighting-stick-ps5-ps4-pc-14424?utm_source=hind.ee&utm_medium=referral",
+    storeName: "gameroom.ee"
+  }
+];
